9de373ba2b9bbe8944287f3a1bab13803a1d4b31
efreestone <efreestone@fullsail.edu>
2015-07-12 13:48:39 -0600

    Still working on it...

--- AstroBlastInvasion/AstroBlastInvasion/MainMenuScene.m
+++ AstroBlastInvasion/AstroBlastInvasion/MainMenuScene.m
@@ -74,6 +74,8 @@
             labelGap = 45;
         }
         
+        //grr what is up with this repo?!
+        
         float labelGapFromFont = fontSize / 2.65;
         
         //Create and set sign in label. Will be modified to say Sign Out when user is logged in
